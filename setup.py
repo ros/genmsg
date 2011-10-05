735087af74cf8b1a2ba71dfe53c209bccb9d2bf4
Ken Conley <kwc@willowgarage.com>
2011-10-05 12:45:50 -0700

    initial commit, in progress

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name='genmsg',
+      version= '0.1.0',
+      packages=['genmsg'],
+      package_dir = {'':'src'},
+      scripts = [],
+      author = "Ken Conley, Josh Faust, Troy Straszheim", 
+      author_email = "kwc@willowgarage.com",
+      url = "http://www.ros.org/wiki/genmsg",
+      download_url = "http://pr.willowgarage.com/downloads/genmsg/", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      description = "ROS msg/srv generation", 
+      long_description = """\
+Library and scripts for generating ROS message data structures in Python, C++, and Lisp.
+""",
+      license = "BSD"
+      )
