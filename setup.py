7a6ed3e7c1292d47141ea7964bad1240dd671562
Dave Hershberger <hersh@willowgarage.com>
2012-09-20 13:54:17 -0700

    setup.py: converted to Dirk's new more streamlined version

--- setup.py
+++ setup.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 from distutils.core import setup
-import sys
-from xml.etree.ElementTree import ElementTree
+from catkin.package import parse_package_for_distutils
 
-try:
-    root = ElementTree(None, 'stack.xml')
-    version = root.findtext('version')
-except Exception as e:
-    print('Could not extract version from your stack.xml:\n%s' % e, file=sys.stderr)
-    sys.exit(-1)
+d = parse_package_for_distutils()
+d['packages'] = ['genmsg']
+d['package_dir'] = {'': 'src'}
 
-sys.path.insert(0, 'src')
-
-setup(name = 'genmsg',
-      version = version,
-      packages = ['genmsg'],
-      package_dir = {'': 'src'},
-      scripts = [],
-      author = "Ken Conley, Josh Faust, Troy Straszheim",
-      author_email = "kwc@willowgarage.com",
-      url = "http://www.ros.org/wiki/genmsg",
-      download_url = "http://pr.willowgarage.com/downloads/genmsg/",
-      keywords = ["ROS"],
-      classifiers = [
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License" ],
-      description = "ROS msg/srv generation",
-      long_description = """\
-Library and scripts for generating ROS message data structures in Python, C++, and Lisp.
-""",
-      license = "BSD"
-      )
+setup(**d)
