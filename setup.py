94a42f30dce2c6ba0e3902a8fc49afe7e1640738
Dirk Thomas <dthomas@willowgarage.com>
2012-09-21 12:56:43 -0700

    updated setup.py and install destinations

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin.package import parse_package_for_distutils
+from catkin_pkg.package import parse_package_for_distutils
 
 d = parse_package_for_distutils()
 d['packages'] = ['genmsg']
